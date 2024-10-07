b9591b6720eb3ec192088928fd67f0cc79af5931
Alejandro <64385399+Fractal-Vortex@users.noreply.github.com>
2024-10-07 18:21:38 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".org", ".es", ".ar"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i of pronoun) {
+  for (let x of adj) {
+    for (let j of noun) {
+      for (let k of domain) {
+        document.write(`<p>${i}${x}${j}${k}</p><hr>`);
+        console.log(`${i}${x}${j}${k}`);
+      }
+    }
+  }
+}
